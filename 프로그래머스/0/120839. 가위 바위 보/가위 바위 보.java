af75c894dbfcdc73493a4a894ec74df25f82d978
Dain <139747705+awesomepossumgirl@users.noreply.github.com>
2025-03-21 16:00:59 +0900

    [level 0] Title: 가위 바위 보, Time: 0.07 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -1,19 +1,6 @@
-
-import java.util.*;
-
 class Solution {
     public String solution(String rsp) {
-        char[] arr = rsp.toCharArray();
-        StringBuilder sb = new StringBuilder();
-        for (char c : arr) {
-            if (c=='2') {
-                sb.append('0');
-            } else if (c=='0') {
-                sb.append('5');
-            } else if (c=='5') {
-                sb.append('2');
-            }
-        }
-        return sb.toString();
+        String answer = "";
+        return answer;
     }
 }
