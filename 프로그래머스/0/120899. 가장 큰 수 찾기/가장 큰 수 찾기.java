6d0023e9db17ad074a6be59d72a6c9e2b1bc904c
Dain <139747705+awesomepossumgirl@users.noreply.github.com>
2025-05-19 14:10:04 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array) {
+        int max = array[0];
+        int idx = 0;
+        
+        for(int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+                idx = i;
+            }
+        }
+        return new int[]{max, idx};
+    }
+}
