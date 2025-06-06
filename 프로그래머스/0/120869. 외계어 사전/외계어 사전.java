a8820864ee325a20efcca80cb1202d279726ebe8
Dain <139747705+awesomepossumgirl@users.noreply.github.com>
2025-06-06 19:44:01 +0900

    [level 0] Title: 외계어 사전, Time: 0.45 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
@@ -1,26 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int solution(String[] spell, String[] dic) {
-        // spell의 문자를 하나씩 이어 붙여서 String으로 변환후
-        // 다시 char[] 타입 배열로 만들기
-        char[] spellToChars = String.join("", spell).toCharArray();
-        // 문자 타입 배열 정렬
-        Arrays.sort(spellToChars);
-        // 문자 배열을 String으로 변환
-        String sortedSpell = new String(spellToChars);
-        
-        // dic 배열의 각 단어 검사
-        for (String word : dic) {
-            // 길이가 같지 않으면 건너뛴다.
-            if (word.length()!= spell.length) continue;
-            // 각 단어를 문자 정렬한다.
-            char[] wordToChar = word.toCharArray();
-            Arrays.sort(wordToChar);
-            String sortedWord = new String(wordToChar);
-            // 정렬된 문자열이 같다면 1 반환
-            if (sortedWord.equals(sortedSpell)) return 1;
-        }
-        return 2;
+        int answer = 0;
+        return answer;
     }
 }
