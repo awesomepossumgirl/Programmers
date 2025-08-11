619d15add13b99163d5905d07f49fdc51e8e704d
Dain <139747705+awesomepossumgirl@users.noreply.github.com>
2025-08-11 11:46:08 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.03 ms, Memory: 83.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(int num) {
+        if (num % 2 == 0) {
+            return new String("Even"); 
+        } else {
+            return new String("Odd"); 
+        }
+    }
+}
