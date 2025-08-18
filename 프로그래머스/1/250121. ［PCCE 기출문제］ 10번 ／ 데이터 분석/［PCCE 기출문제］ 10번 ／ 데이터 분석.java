d0f25e65a3d2ca4bc3b91be52eadcc6a5b49967c
Dain <139747705+awesomepossumgirl@users.noreply.github.com>
2025-08-18 16:09:19 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 15.25 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -1,41 +1,21 @@
-// 1. ext < val_ext 만 list에 추가 - 2차원 리스트?
-// 2. sort_by 기준 정렬 - 람다
 import java.util.*;
 
 class Solution {
     public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
-        List<int[]> list = new ArrayList<>();
-        int idx = 0; // 필터 인덱스
-        int sortIdx = 0; // 정렬 인덱스
-        switch (ext) { // 필터 기준
-            case "code":
-                idx = 0; break;
-            case "date":
-                idx = 1; break;
-            case "maximum":
-                idx = 2; break;
-            case "remain":
-                idx = 3; break;
-        }
-        switch (sort_by) { // 정렬 기준
-            case "code":
-                sortIdx = 0; break;
-            case "date":
-                sortIdx = 1; break;
-            case "maximum":
-                sortIdx = 2; break;
-            case "remain":
-                sortIdx = 3; break;
-        }
-        // 조건에 맞는 데이터만 추가
-        for(int i = 0; i < data.length; i++) {
-            if (data[i][idx] < val_ext) list.add(data[i]);
-        }
-        // 람다에서 사용하기 위해 final 변수 선언
-        final int sortIndex = sortIdx;
-        // 람다식 정렬
-        list.sort((a, b) -> Integer.compare(a[sortIndex], b[sortIndex]));
+        // 문자열, 인덱스 매핑
+        Map<String, Integer> map = Map.of(
+            "code", 0,
+            "date", 1,
+            "maximum", 2,
+            "remain", 3
+        );
         
-        return list.toArray(new int[list.size()][]);
+        int idx = map.get(ext); // 필터 인덱스
+        int sortIdx = map.get(sort_by); // 정렬 인덱스
+        
+        return Arrays.stream(data)
+                .filter(arr -> arr[idx] < val_ext) // 조건 맞는 애들만 필터링
+                .sorted(Comparator.comparingInt(a -> a[sortIdx])) // 정렬
+                .toArray(int[][]::new); // 배열로 변환
     }
 }
