05c67a3fd36946a60e0a116a23254edf1b5eab87
Dain <139747705+awesomepossumgirl@users.noreply.github.com>
2025-08-27 10:15:20 +0900

    [level 2] Title: 짝지어 제거하기, Time: 69.60 ms, Memory: 65 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -1,21 +1,12 @@
-import java.util.*;
-
 class Solution
 {
     public int solution(String s)
     {
-        Stack<Character> stack = new Stack<>();
-        
-        for (char c : s.toCharArray()) {
-            if (!stack.isEmpty() && stack.peek() == c) { // 스택이 비어있지 않고
-                stack.pop(); // 같은 문자가 연속 2번 나오면 제거
-            } else {
-                stack.push(c);
-            }
-        }
-        
-        // 스택 비어 있으면 짝수쌍임
-        return stack.isEmpty() ? 1 : 0;
-        
+        int answer = -1;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
     }
 }
